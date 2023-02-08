3d698d5e41ac425e9c40f877ad908223269fae5f
lilit1121 <tamrazyan.lilit.95@gmail.com>
2023-02-08 11:12:47 +0400

    empty app

---
+++ .history/src/views/HomeView_20230208111034.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+  he
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  
+}
+</script>
---
+++ .history/src/views/Home_20230208111204.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+  Rapid Prototyping
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  
+}
+</script>
